4735bf4f591e26a99a96fa2fcb8d18425d7ed8f2
ohgnus56 <44199899+ohgnus56@users.noreply.github.com>
2024-02-15 23:19:25 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,12 +8,8 @@ thread_id = st.secrets["thread_id"]
 test_text = st.secrets["test_text"]
 
 with st.sidebar:
-    components.html(
-    """
-    <iframe src="https://ads-partners.coupang.com/widgets.html?id=757657&template=carousel&trackingCode=AF1960684&subId=&width=300&height=300&tsource=" width="300" height="300" frameborder="0" scrolling="no" referrerpolicy="unsafe-url" browsingtopics></iframe>
-    """,
-    # height=600,
-)
+    link = 'http://www.coupang.com'
+    st.markdown(f'[Example Link]({link})', unsafe_allow_html=True)
     st.title(f":blue[{test_text}]")
     openai_api_key = st.text_input("OpenAI API KEY", type="password")
 
